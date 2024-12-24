a0f54d0ff8af6cbfb1a9faa706903652a6335aa9
lsp <2090395279@qq.com>
2024-12-24 08:52:14 +0800

    新增咨询信息

--- src/main/java/com/example/dao/NewsInfoDao.java
+++ src/main/java/com/example/dao/NewsInfoDao.java
@@ -2,11 +2,14 @@ package com.example.dao;
 
 
 import com.example.pojo.NewsInfo;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface NewsInfoDao {
+@Repository
+public interface NewsInfoDao extends Mapper<NewsInfo> {
     //获取所有饮食咨询
     @Select("SELECT * FROM news_info")
     List<NewsInfo> findAll();
