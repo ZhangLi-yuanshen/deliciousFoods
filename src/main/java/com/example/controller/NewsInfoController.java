c6e53feeb1d100150f73e66daf3fa039675e3fc2
lsp <2090395279@qq.com>
2024-12-24 08:30:49 +0800

    新增咨询信息

--- src/main/java/com/example/controller/NewsInfoController.java
+++ src/main/java/com/example/controller/NewsInfoController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.List;
 
-
 @RestController
+
 @RequestMapping("/NewsInfo")
 public class NewsInfoController {
     @Resource
