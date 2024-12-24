56da068dcce6fe4355ca9b6722c278c2b47f3ed3
lsp <2090395279@qq.com>
2024-12-24 08:17:04 +0800

    新增咨询信息

--- src/main/java/com/example/controller/NewsInfoController.java
+++ src/main/java/com/example/controller/NewsInfoController.java
@@ -5,7 +5,6 @@ package com.example.controller;
 import com.example.common.Result;
 import com.example.pojo.NewsInfo;
 import com.example.service.NewsInfoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
