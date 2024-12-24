a2a6a378394a823ab31fdc6c9ccb19b8f497bdc7
Wangxia <2470276367@qq.com>
2024-12-24 11:31:23 +0800

    查询

--- src/main/java/com/example/controller/wxAdminInfoController.java
+++ src/main/java/com/example/controller/wxAdminInfoController.java
@@ -1,9 +1,24 @@
 package com.example.controller;
 
+import com.example.service.AdminInfoService;
+import com.example.vo.AdminVo;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RestController
-@RequestMapping("/AdminInfo")
+@RequestMapping("/adminInfo")
 public class wxAdminInfoController {
+    @Resource
+    private AdminInfoService adminInfoService;
+    //获取管理员信息
+    @GetMapping
+    public List<AdminVo> getAdminInfo(){
+        return adminInfoService.selectAll();
+    }
+
+
 }
