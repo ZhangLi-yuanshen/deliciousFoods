1479c19d816c95b94b5900a38a980d45f1dfae24
lsp <2090395279@qq.com>
2024-12-25 09:57:23 +0800

    新增咨询信息

--- src/main/java/com/example/mapper/NewsInfoMapper.java
+++ src/main/java/com/example/mapper/NewsInfoMapper.java
@@ -1,10 +1,12 @@
 package com.example.mapper;
 
 import com.example.pojo.NewsInfo;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
+@Mapper
 public interface NewsInfoMapper {
     /**
      * 统计总记录数
