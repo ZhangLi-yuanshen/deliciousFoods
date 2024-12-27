2697498d9c6b160cedb10a59d78eeeef3823a540
ZhaangLi <18072416561@163.com>
2024-12-27 10:35:49 +0800

    Changes

--- src/main/java/com/example/controller/NewsInfoController.java
+++ src/main/java/com/example/controller/NewsInfoController.java
@@ -2,31 +2,30 @@ package com.example.controller;
 
 
 
-import com.example.common.QueryRequest;
 import com.example.common.Result;
 import com.example.pojo.NewsInfo;
-import com.example.service.impl.NewsInfoServiceImpl;
-import com.example.util.PageResult;
-import com.example.util.ResponseUtils;
+import com.example.service.NewsInfoService;
 import com.example.vo.NewsInfoVo;
+import com.github.pagehelper.PageInfo;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
+
 @RequestMapping("/newsInfo")
 public class NewsInfoController {
     @Resource
-    private NewsInfoServiceImpl newsInfoService;
-    // 获取所有新闻资讯（分页）
-    @GetMapping("/page/{name}")
-    public ResponseUtils list(@RequestBody QueryRequest queryRequest){
-        PageResult<NewsInfo> newsInfoPageResult = newsInfoService.selectNewsInfoList(queryRequest);
-        return new ResponseUtils(200,"success",newsInfoPageResult);
-    }
-    //查询所有咨询信息
+    private NewsInfoService newsInfoService;
     @GetMapping
     public Result<List<NewsInfoVo>> all(){return Result.success(newsInfoService.findAll());}
-
+//    @GetMapping("/page/{name}")
+//    public Result<PageInfo<NewsInfoVo>> page(@PathVariable String name,
+//                                             @RequestParam(defaultValue = "1") Integer pageNum,
+//                                             @RequestParam(defaultValue = "5") Integer pageSize,
+//                                             HttpServletRequest request) {
+//        return Result.success(newsInfoService.findPage(name, pageNum, pageSize, request));
+//    }
 }
--- src/main/java/com/example/service/NewsInfoService.java
+++ src/main/java/com/example/service/NewsInfoService.java
@@ -1,13 +1,29 @@
 package com.example.service;
 
-
-import com.example.common.QueryRequest;
+import com.example.dao.NewsInfoDao;
 import com.example.pojo.NewsInfo;
-import com.example.util.PageResult;
+import com.example.vo.NewsInfoVo;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
-public interface NewsInfoService {
-    /**
-     * 分页查询相关数据
-     */
-    PageResult<NewsInfo> selectNewsInfoList(QueryRequest queryRequest);
+@Service
+public class NewsInfoService {
+    //获取所有饮食咨询
+    @Resource
+    private NewsInfoDao newsInfoDao;
+    public List<NewsInfoVo> findAll() {
+        return newsInfoDao.findAll("all");
+    }
+    public PageInfo<NewsInfoVo> findPage(String name, Integer pageNum, Integer pageSize, HttpServletRequest request) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<NewsInfoVo> all = findAllPage(request, name);
+        return PageInfo.of(all);
+    }
+    public List<NewsInfoVo> findAllPage(HttpServletRequest request, String name) {
+        return newsInfoDao.findAll(name);
+    }
 }
