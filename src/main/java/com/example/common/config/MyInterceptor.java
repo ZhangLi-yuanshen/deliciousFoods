cd7163a8d778c96fdc23b0fee03eccca4e67ba64
ZhaangLi <18072416561@163.com>
2025-01-02 10:59:10 +0800

    Changes

--- src/main/java/com/example/common/config/MyInterceptor.java
+++ src/main/java/com/example/common/config/MyInterceptor.java
@@ -12,17 +12,17 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class MyInterceptor implements HandlerInterceptor {
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse
-            response, Object handler) throws Exception {
-        Account user = (Account) request.getSession().getAttribute("user");
-        if (user == null) {
-            // 重定向到登录页
-            response.sendRedirect("/end/page/login.html");
-            return false;
-        }
-        return true;
-    }
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse
+//            response, Object handler) throws Exception {
+//        Account user = (Account) request.getSession().getAttribute("user");
+//        if (user == null) {
+//            // 重定向到登录页
+//            response.sendRedirect("/end/page/login.html");
+//            return false;
+//        }
+//        return true;
+//    }
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse
             response, Object handler, ModelAndView modelAndView) throws Exception {
