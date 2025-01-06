3302f1b60120010076616d48e5a26cb8d3c3d4db
lsp <2090395279@qq.com>
2025-01-06 18:15:50 +0800

    新增咨询信息

--- src/main/java/com/example/vo/NewsInfoVo.java
+++ src/main/java/com/example/vo/NewsInfoVo.java
@@ -1,13 +1,7 @@
 package com.example.vo;
 
-public class NewsInfoVo {
-    private Long id;
-    private String name;
-    private String content;
-    // 构造函数
-    public NewsInfoVo(Long id, String name, String content) {
-        this.id = id;
-        this.name = name;
-        this.content = content;
-    }
+import com.example.entity.NewsInfo;
+
+public class NewsInfoVo extends NewsInfo {
+
 }
