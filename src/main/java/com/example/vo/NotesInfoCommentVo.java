9c8e2e57cb9ddf4e8151003bda70f11f42ce573d
ZhaangLi <18072416561@163.com>
2025-01-09 20:40:10 +0800

    修改

--- src/main/java/com/example/vo/NotesInfoCommentVo.java
+++ src/main/java/com/example/vo/NotesInfoCommentVo.java
@@ -1,16 +1,13 @@
 package com.example.vo;
 
 import com.example.entity.CommentNotesInfo;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class NotesInfoCommentVo extends CommentNotesInfo {
 
     private String foreignName;
 
-    public String getForeignName() {
-        return foreignName;
-    }
-
-    public void setForeignName(String foreignName) {
-        this.foreignName = foreignName;
-    }
 }
