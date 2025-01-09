03bb24aaa55393a2ae644d3518283f68f2354a36
ZhaangLi <18072416561@163.com>
2025-01-09 11:15:53 +0800

    数据计数

---
+++ src/main/java/com/example/vo/ClassifyInfoVo.java
@@ -0,0 +1,6 @@
+package com.example.vo;
+
+import com.example.entity.ClassifyInfo;
+
+public class ClassifyInfoVo extends ClassifyInfo {
+}
---
+++ src/main/java/com/example/vo/SubClassifyInfoVo.java
@@ -0,0 +1,6 @@
+package com.example.vo;
+
+import com.example.entity.SubClassifyInfo;
+
+public class SubClassifyInfoVo extends SubClassifyInfo {
+}
