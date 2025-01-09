fc7f43fcd3afa7e56a52ab53569d21b1e624c1a9
ZhaangLi <18072416561@163.com>
2025-01-09 21:25:45 +0800

    菜谱信息

--- src/main/java/com/example/vo/CollectInfoVo.java
+++ src/main/java/com/example/vo/CollectInfoVo.java
@@ -1,16 +1,13 @@
 package com.example.vo;
 
 import com.example.entity.CollectInfo;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class CollectInfoVo extends CollectInfo {
 
     private String foodsName;
-
-    public String getFoodsName() {
-        return foodsName;
-    }
-    public void setFoodsName(String foodsName) {
-        this.foodsName = foodsName;
-    }
 
 }
