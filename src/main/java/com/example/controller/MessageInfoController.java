988ec3bf939be620f0b40b42b399c7a325dd0add
Wangxia <2470276367@qq.com>
2025-01-10 10:07:12 +0800

    笔记信息评论

--- src/main/java/com/example/controller/MessageInfoController.java
+++ src/main/java/com/example/controller/MessageInfoController.java
@@ -1,5 +1,9 @@
 package com.example.controller;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
 import com.example.common.Result;
 import com.example.entity.MessageInfo;
 import com.example.service.MessageInfoService;
@@ -8,8 +12,13 @@ import com.github.pagehelper.PageInfo;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/messageInfo")
@@ -60,5 +69,30 @@ public Result update(@RequestBody MessageInfoVo messageInfo) {
                                                 @RequestParam(defaultValue = "5") Integer pageSize,
                                                 HttpServletRequest request) {
         return Result.success(messageInfoService.findPage(name, pageNum, pageSize, request));
+    }
+    @GetMapping("/getExcelModel")
+    public void getExcelModel(HttpServletResponse response) throws IOException {
+        // 1. 生成excel
+        Map<String, Object> row = new LinkedHashMap<>();
+        row.put("name", "张三");
+        row.put("password", "123456");
+        row.put("nickName", "昵称");
+        row.put("sex", "男");
+        row.put("age", 22);
+        row.put("level", 1);
+
+        List<Map<String, Object>> list = CollUtil.newArrayList(row);
+
+        // 2. 写excel
+        ExcelWriter writer = ExcelUtil.getWriter(true);
+        writer.write(list, true);
+
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8");
+        response.setHeader("Content-Disposition","attachment;filename=message.xlsx");
+
+        ServletOutputStream out = response.getOutputStream();
+        writer.flush(out, true);
+        writer.close();
+        IoUtil.close(System.out);
     }
 }
--- src/main/java/com/example/controller/UserInfoController.java
+++ src/main/java/com/example/controller/UserInfoController.java
@@ -1,18 +1,27 @@
 package com.example.controller;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
 import com.example.common.Result;
-import com.example.entity.AdminInfo;
+
 import com.example.entity.UserInfo;
-import com.example.service.AdminInfoService;
+
 import com.example.service.UserInfoService;
-import com.example.vo.AdminInfoVo;
+
 import com.example.vo.UserInfoVo;
 import com.github.pagehelper.PageInfo;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/userInfo")
@@ -55,5 +64,36 @@ public class UserInfoController {
                                               @RequestParam(defaultValue = "5") Integer pageSize,
                                               HttpServletRequest request) {
         return Result.success(userInfoService.findPage(name, pageNum, pageSize, request));
+    }
+    @GetMapping("/getExcelModel")
+    public void getExcelModel(HttpServletResponse response) throws IOException {
+        // 1. 生成excel
+        Map<String, Object> row = new LinkedHashMap<>();
+        row.put("name", "//导出用户");
+        row.put("password", "123456");
+        row.put("nickName", "//导出用户");
+        row.put("sex", "男");
+        row.put("age", 22);
+        row.put("birthday", "TIME");
+        row.put("phone", "18843232356");
+        row.put("address", "上海市");
+        row.put("code", "111");
+        row.put("email", "aa@163.com");
+        row.put("cardId", "342425199001116372");
+        row.put("level", 1);
+
+        List<Map<String, Object>> list = CollUtil.newArrayList(row);
+
+        // 2. 写excel
+        ExcelWriter writer = ExcelUtil.getWriter(true);
+        writer.write(list, true);
+
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8");
+        response.setHeader("Content-Disposition","attachment;filename=user.xlsx");
+
+        ServletOutputStream out = response.getOutputStream();
+        writer.flush(out, true);
+        writer.close();
+        IoUtil.close(System.out);
     }
 }
